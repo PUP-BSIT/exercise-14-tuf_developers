f75097e1b4118ced93cc496eb7cdd443caeb7f93
nogadas-vonryan <vonnogadas251@gmail.com>
2024-06-04 19:48:43 +0800

    add functions to get countries with fetch

--- nogadas_vonryan/countries/countries.js
+++ nogadas_vonryan/countries/countries.js
@@ -1,3 +1,13 @@
 const country = document.querySelector("#country");
 const sameRegion = document.querySelector("#same_region");
 const inputCountry = document.querySelector("#input_country");
+
+function getCountry() {
+    displayLoading();
+	fetch(`https://restcountries.com/v3.1/name/${inputCountry.value}`);
+}
+
+
+function getCountriesOfSameRegion(region) {
+    fetch(`https://restcountries.com/v3.1/region/${region}`);
+}
